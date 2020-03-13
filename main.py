e1f61b0455dbe68ecf39ce62d242f0224846d38a
Greg Walker <mgwalker@gmail.com>
2020-03-12 22:22:01 -0500

    more stats

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import chevron
+from math import log
 import requests
 
 time_series = requests.get("https://covidtracking.com/api/us/daily").json()
@@ -9,14 +10,39 @@ time_series = [
 prev = 0
 for date in time_series:
     if prev == 0:
+        date["change_percent"] = 0
         date["change"] = 0
         prev = date["total"]
         continue
 
+    date["change_percent"] = (date["total"] / prev) - 1
     date["change"] = round((date["total"] / prev) - 1, 4) * 100
     prev = date["total"]
 
-latest_change = time_series.pop()["change"]
+latest_change = time_series[len(time_series) - 1]["change"]
+
+daily_average_change = sum([day["change_percent"] for day in time_series]) / (
+    len(time_series) - 1
+)
+
+recent_days = 3
+recent_average_change = (
+    sum([day["change_percent"] for day in time_series[-recent_days:]]) / recent_days
+)
+
+doubling = round(log(2, 1 + daily_average_change), 2)
+doubling_recent = round(log(2, 1 + recent_average_change), 2)
+
+projections_all = {
+    "10_days": f'{round(time_series[-1]["total"] * ((1 + daily_average_change) ** 10)):,}',
+    "20_days": f'{round(time_series[-1]["total"] * ((1 + daily_average_change) ** 20)):,}',
+    "30_days": f'{round(time_series[-1]["total"] * ((1 + daily_average_change) ** 30)):,}',
+}
+projections_recent = {
+    "10_days": f'{round(time_series[-1]["total"] * ((1 + recent_average_change) ** 10)):,}',
+    "20_days": f'{round(time_series[-1]["total"] * ((1 + recent_average_change) ** 20)):,}',
+    "30_days": f'{round(time_series[-1]["total"] * ((1 + recent_average_change) ** 30)):,}',
+}
 
 states = requests.get("https://covidtracking.com/api/states").json()
 
@@ -44,7 +70,16 @@ with open("index.mustache", "r") as template:
     with open("index.html", "w") as index:
         index.write(
             chevron.render(
-                template, {"percent_change": latest_change, "total": us_total}
+                template,
+                {
+                    "doubling": doubling,
+                    "doubling_recent": doubling_recent,
+                    "percent_change": latest_change,
+                    "projections_all": projections_all,
+                    "projections_recent": projections_recent,
+                    "recent_days": recent_days,
+                    "total": f"{us_total:,}",
+                },
             )
         )
         index.close()

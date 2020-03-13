cf745dcf36588950e922cec6524030aa4a095881
Greg Walker <mgwalker@gmail.com>
2020-03-13 07:43:36 -0500

    maybe try actually pushing automatic changes...

--- .github/workflows/git.sh
+++ .github/workflows/git.sh
@@ -6,7 +6,7 @@ then
   git config --local user.email "mgwalker@users.noreply.github.com"
   git config --local user.name "automatic update"
   git commit -m "regular update" -a
-  git remote set-url --push origin https://mgwalker:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY
+  git push "https://mgwalker:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY" HEAD:master
 else
   echo "no changes detected; skipping"
 fi

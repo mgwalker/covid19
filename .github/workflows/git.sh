d8021424b0cd2da5fc1827583578933c7be00553
Greg Walker <mgwalker@gmail.com>
2020-03-12 22:32:21 -0500

    add a message so we know what happened

--- .github/workflows/git.sh
+++ .github/workflows/git.sh
@@ -2,8 +2,11 @@
 
 if [[ `git status --porcelain` ]]
 then
+  echo "committing and pushing changes"
   git config --local user.email "mgwalker@users.noreply.github.com"
   git config --local user.name "automatic update"
   git commit -m "regular update" -a
   git remote set-url --push origin https://mgwalker:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY
+else
+  echo "no changes detected; skipping"
 fi

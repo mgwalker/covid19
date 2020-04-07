64df319afb41ad3b4baeb966ac80dc8d4831a0ed
Greg Walker <mgwalker@gmail.com>
2020-04-07 18:44:27 -0500

    more text

--- build/template.py
+++ build/template.py
@@ -29,6 +29,28 @@ def write_data_to_template(data, file, index=False, extra_data={}):
         positive = data[-1]["positive"]
         proportion = positive / local_population
 
+        increase = data[-1]["positiveIncrease"]
+        increase_rate = (
+            round(100 * increase / data[-2]["positive"], 1)
+            if data[-2]["positive"] > 0
+            else 0
+        )
+        increase_yesterday = data[-2]["positiveIncrease"]
+        increase_yesterday_rate = (
+            round(100 * increase_yesterday / data[-3]["positive"], 1)
+            if data[-3]["positive"] > 0
+            else 0
+        )
+
+        increase_diff = increase_rate - increase_yesterday_rate
+        increase_change = (
+            "about the same as"
+            if abs(increase_diff) < 0.2
+            else ("a little higher than" if increase_diff < 1 else "higher than")
+            if increase_diff > 0
+            else ("a little lower than" if increase_diff > -1 else "lower than")
+        )
+
         per = 1
         while proportion < 1 and proportion > 0:
             proportion *= 10
@@ -42,17 +64,22 @@ def write_data_to_template(data, file, index=False, extra_data={}):
                         template,
                         {
                             **extra_data,
-                            "index": index,
-                            "location": location_name,
-                            "total": f"{positive:,}",
-                            "proportion": {"count": proportion, "per": f"{per:,}"},
+                            "all_states": all_states,
                             "change_rate": {
                                 "doubling": round(doubling_recent, 1),
                                 "rate": round((recent_average_change - 1) * 100, 1),
                                 "seven_days": f"{round(seven_days):,}",
                             },
+                            "increase": f"{increase:,}",
+                            "increase_change": increase_change,
+                            "increase_rate": increase_rate,
+                            "increase_yesterday": f"{increase_yesterday:,}",
+                            "increase_yesterday_rate": increase_yesterday_rate,
+                            "index": index,
+                            "location": location_name,
+                            "proportion": {"count": proportion, "per": f"{per:,}"},
                             "short_name": "USA" if index else data[0]["state"],
-                            "all_states": all_states,
+                            "total": f"{positive:,}",
                         },
                     )
                 )

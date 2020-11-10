c09d2c715939cb454a2e391f1a69c74159460057
Greg Walker <mgwalker@gmail.com>
2020-11-09 20:58:39 -0600

    fix git script

--- .github/workflows/git.sh
+++ .github/workflows/git.sh
@@ -6,7 +6,7 @@ then
   git config --local user.email "mgwalker@users.noreply.github.com"
   git config --local user.name "automatic update"
   git commit -am "regular update"
-  git push "https://mgwalker:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY" HEAD:master
+  git push "https://mgwalker:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY" HEAD:main
 else
   echo "no changes detected; skipping"
 fi
